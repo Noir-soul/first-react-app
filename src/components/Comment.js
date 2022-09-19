ff7ec9e0cd18310a5a09557821298a13e52ee491
Jethro Carreon <shirocarreon@gmail.com>
2022-09-19 14:55:52 +0800

    added a non working delete comment button

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -5,13 +5,14 @@ class Comment extends Component {
     render() {
         return(
             <div className="comment">
-                <p className="comment-header">{this.props.author}</p>
-                <p className="comment-body">{this.props.body}</p>
+                <p className="comment-header">{this.props.comment.author}</p>
+                <p className="comment-body">{this.props.comment.body}</p>
                 <div className="comment-footer">
-                    <a className="comment-footer-delete">
-                        Delete comment
-                    </a>
+                    <button className="comment-footer-delete">
+                        Delete
+                    </button>
                 </div>
+                <hr></hr>
             </div>
         )
     }
@@ -21,23 +22,20 @@ class CommentBox extends Component {
 
     state = {
         showComments: false,
-        commentShown: "Hide coments",
-        commentHidden: "Show comments",
-    }
-
-    _getComments() {
-        let commentList = [
+        commentList: [
             {id: 1, author: 'Morgan McCircuit', body: 'Great app'},
             {id: 2, author: 'Bending Bender', body: 'Excellent stuff'},
             {id: 3, author: 'Deez Nuts', body: 'good good good'},
             {id: 4, author: 'Bat Man', body: 'hmmm'},
-        ];
-
-        return commentList.map((comment) => {
-            return( <Comment author={comment.author} body={comment.body} key={comment.id}/>)
-        })
+        ]
     }
 
+    _getComments() {
+        return this.state.commentList.map((comment) => {
+            return( <Comment comment={comment} key={comment.id}/>)
+        })
+    }
+    
     _getCommentsTitle(count) {
          return (count == 0) ? "No comments yet" : count == 1 ? "1 comment" : `${count} comments`
     }
@@ -47,7 +45,7 @@ class CommentBox extends Component {
         let commentNodes;
         let buttonText = "Show comments";
         (this.state.showComments) ? ((commentNodes = <div className="comment-list">{comments}</div>) && (buttonText = "Hide comments")) : commentNodes;
-        
+
         return(
             <div className="comment-box">
                 <h4 className="comment-count">{this._getCommentsTitle(comments.length)}</h4>
